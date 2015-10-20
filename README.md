9ffbd498cc6227e28971e0189c6ddceaff3b74ed
Ryan Macdonald <macdonaldr93@gmail.com>
2015-10-20 15:48:08 -0400

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
 # yamm-nav-walker
 A custom WordPress nav walker class to implement the Yamm 3 bootstrap mega menu navigation style in a custom theme using the WordPress built in menu manager.
+
+##NOTE
+
+This is a utility class that is intended to format your WordPress theme menu with the correct syntax and classes to utilize the Bootstrap dropdown navigation, and does not include the required Bootstrap JS files or Yamm 3 files. You will have to include them manually.
+
+- http://getbootstrap.com/getting-started/#download
+- http://geedmo.github.io/yamm3/
+
+## Installation
+
+Place wp_bootstrap_navwalker.php in your WordPress theme folder /wp-content/your-theme/
+
+Open your WordPress themes functions.php file /wp-content/your-theme/functions.php and add the following code:
+```php
+// Register Custom Navigation Walker
+require_once('wp_bootstrap_navwalker.php');
+```
+
+## Usage
+
+Update your `wp_nav_menu()` function in `header.php` to use the new walker by adding a "walker" item to the wp_nav_menu array.
+
+```php
+ <?php
+            wp_nav_menu( array(
+                'menu'              => 'primary',
+                'theme_location'    => 'primary',
+                'depth'             => 4,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+		        'container_id'      => 'bs-example-navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav yamm',
+                'fallback_cb'       => 'Yamm_Nav_Walker_menu_fallback',
+                'walker'            => new Yamm_Nav_Walker())
+            );
+        ?>
+```
+
+Your menu will now be formatted with the correct syntax and classes to implement Bootstrap dropdown navigation. 
+
+You will also want to declare your new menu in your `functions.php` file.
+
+```php
+register_nav_menus( array(
+	'primary' => __( 'Primary Menu', 'THEMENAME' ),
+) );
+```
+
+Typically the menu is wrapped with additional markup, here is an example of a `navbar-fixed-top` menu that collapse for responsive navigation.
+
+```php
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>">
+                <?php bloginfo('name'); ?>
+            </a>
+    </div>
+
+        <?php
+            wp_nav_menu( array(
+                'menu'              => 'primary',
+                'theme_location'    => 'primary',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+		        'container_id'      => 'bs-example-navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'Yamm_Nav_Walker_menu_fallback',
+                'walker'            => new Yamm_Nav_Walker())
+            );
+        ?>
+    </div>
+</nav>
+```
+
+To change your menu style add Bootstrap nav class names to the `menu_class` declaration.
+
+Review options in the Bootstrap docs for more information on nav classes
+http://getbootstrap.com/components/#nav
+
+You can also use the full width walker like so:
+
+```php
+ <?php
+            wp_nav_menu( array(
+                'menu'              => 'primary',
+                'theme_location'    => 'primary',
+                'depth'             => 4,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+		        'container_id'      => 'bs-example-navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav yamm',
+                'fallback_cb'       => 'Yamm_Fw_Nav_Walker_menu_fallback',
+                'walker'            => new Yamm_Fw_Nav_Walker())
+            );
+        ?>
+```
---
+++ yamm-nav-walker.php
@@ -0,0 +1,256 @@
+<?php
+/**
+ * Class Name: Yamm_Nav_Walker & Yamm_Fw_Nav_Walker
+ * GitHub URI: https://github.com/macdonaldr93/yamm-nav-walker
+ * Description: A custom WordPress nav walker class to implement the Yamm 3 bootstrap mega menu navigation style in a custom theme using the WordPress built in menu manager.
+ * Version: 1.0.0
+ * Author: Ryan Macdonald - @macdonaldr93
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+class Yamm_Nav_Walker extends Walker_Nav_Menu
+{
+    function check_current($classes)
+    {
+        return preg_match('/(current[-_])|active|dropdown/', $classes);
+    }
+
+    function start_lvl(&$output, $depth, $args = array())
+    {
+        $output .= ($depth == 0) ? "\n<ul class=\"dropdown-menu\">\n" . "\n<div class=\"yamm-content\">\n" . "\n<div class=\"row\">\n" : "\n<ul class=\"elementy-ul yamm-fw\">\n";
+    }
+
+
+    function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0)
+    {
+        $item_html = '';
+        parent::start_el($item_html, $item, $depth, $args);
+
+        if ($item->is_dropdown && ($depth === 0))
+        {
+            $item_html = str_replace('<a', '<a class="dropdown-toggle" data-toggle="dropdown" data-target="#"', $item_html);
+            $item_html = str_replace('</a>', ' <b class="caret"></b></a>', $item_html);
+        }
+
+        elseif (stristr($item_html, 'li class="divider'))
+        {
+            $item_html = preg_replace('/<a[^>]*>.*?<\/a>/iU', '', $item_html);
+        }
+        elseif (stristr($item_html, 'li class="dropdown-header'))
+        {
+            $item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html);
+        }
+
+        $item_html = apply_filters('roots_wp_nav_menu_item', $item_html);
+        $output .= $item_html;
+    }
+
+
+    function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output)
+    {
+
+        $element->is_dropdown = ((!empty($children_elements[$element->ID]) && (($depth + 1) < $max_depth || ($max_depth === 0))));
+        if ($element->is_dropdown)
+        {
+            $element->classes[] = 'dropdown';
+
+
+        }
+        if ($element && ($depth === 1))
+        {
+            $element->classes[] = 'col-sm-4 menu-col';
+        }
+
+        parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+    }
+
+}
+
+/**
+ * Remove the id="" on nav menu items
+ * Return 'menu-slug' for nav menu classes
+ */
+function yamm_roots_nav_menu_css_class($classes, $item)
+{
+    $slug    = sanitize_title($item->title);
+    $classes = preg_replace('/(current(-menu-|[-_]page[-_])(item|parent|ancestor))/', 'active', $classes);
+    $classes = preg_replace('/^((menu|page)[-_\w+]+)+/', '', $classes);
+
+    $classes[] = 'menu-' . $slug;
+
+    $classes = array_unique($classes);
+
+    return array_filter($classes);
+}
+add_filter('nav_menu_css_class', 'yamm_roots_nav_menu_css_class', 10, 2);
+add_filter('nav_menu_item_id', '__return_null');
+
+/**
+ * Clean up wp_nav_menu_args
+ *
+ * Remove the container
+ * Use Yamm_Nav_Walker() by default
+ */
+function yamm_roots_nav_menu_args($args = '')
+{
+    $yamm_roots_nav_menu_args['container'] = false;
+
+    if (!$args['items_wrap'])
+    {
+        $yamm_roots_nav_menu_args['items_wrap'] = '<ul class="%2$s">%3$s</ul>';
+    }
+
+    if (current_theme_supports('bootstrap-top-navbar') && !$args['depth'])
+    {
+        $yamm_roots_nav_menu_args['depth'] = 3;
+    }
+
+    if (!$args['walker'])
+    {
+        $yamm_roots_nav_menu_args['walker'] = new Yamm_Nav_Walker();
+    }
+
+    return array_merge($args, $yamm_roots_nav_menu_args);
+}
+add_filter('wp_nav_menu_args', 'yamm_roots_nav_menu_args');
+
+/**
+ * Fallback to simple list
+ */
+function Yamm_Nav_Walker_menu_fallback()
+{
+    $fb_output = '<ul class="nav navbar-nav">';
+    $fb_output .= '<li><a href="' . esc_url(home_url()) . '">' . __('Home', THEMETD) . '</a></li>';
+    if (current_user_can('manage_options'))
+    {
+        $fb_output .= '<li><a href="' . admin_url('nav-menus.php') . '">' . __('Add a menu', THEMETD) . '</a></li>';
+    }
+    $fb_output .= '</ul>';
+
+    echo $fb_output;
+}
+
+class Yamm_Fw_Nav_Walker extends Walker_Nav_Menu
+{
+    function check_current($classes)
+    {
+        return preg_match('/(current[-_])|active|dropdown/', $classes);
+    }
+
+    function start_lvl(&$output, $depth, $args = array())
+    {
+        $output .= ($depth == 0) ? "\n<ul class=\"dropdown-menu\">\n" . "\n<div class=\"yamm-content\">\n" . "\n<div class=\"row\">\n" : "\n<ul class=\"elementy-ul yamm-fw\">\n";
+    }
+
+
+    function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0)
+    {
+        $item_html = '';
+        parent::start_el($item_html, $item, $depth, $args);
+
+        if ($item->is_dropdown && ($depth === 0))
+        {
+            $item_html = str_replace('<a', '<a class="dropdown-toggle" data-toggle="dropdown" data-target="#"', $item_html);
+            $item_html = str_replace('</a>', ' <b class="caret"></b></a>', $item_html);
+        }
+
+        elseif (stristr($item_html, 'li class="divider'))
+        {
+            $item_html = preg_replace('/<a[^>]*>.*?<\/a>/iU', '', $item_html);
+        }
+        elseif (stristr($item_html, 'li class="dropdown-header'))
+        {
+            $item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html);
+        }
+
+        $item_html = apply_filters('roots_wp_nav_menu_item', $item_html);
+        $output .= $item_html;
+    }
+
+
+    function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output)
+    {
+
+        $element->is_dropdown = ((!empty($children_elements[$element->ID]) && (($depth + 1) < $max_depth || ($max_depth === 0))));
+        if ($element->is_dropdown)
+        {
+            $element->classes[] = 'dropdown yamm-fw';
+
+
+        }
+        if ($element && ($depth === 1))
+        {
+            $element->classes[] = 'col-sm-4 menu-col';
+        }
+
+        parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+    }
+
+}
+
+/**
+ * Remove the id="" on nav menu items
+ * Return 'menu-slug' for nav menu classes
+ */
+function yamm_fw_roots_nav_menu_css_class($classes, $item)
+{
+    $slug    = sanitize_title($item->title);
+    $classes = preg_replace('/(current(-menu-|[-_]page[-_])(item|parent|ancestor))/', 'active', $classes);
+    $classes = preg_replace('/^((menu|page)[-_\w+]+)+/', '', $classes);
+
+    $classes[] = 'menu-' . $slug;
+
+    $classes = array_unique($classes);
+
+    return array_filter($classes);
+}
+add_filter('nav_menu_css_class', 'yamm_fw_roots_nav_menu_css_class', 10, 2);
+add_filter('nav_menu_item_id', '__return_null');
+
+/**
+ * Clean up wp_nav_menu_args
+ *
+ * Remove the container
+ * Use Yamm_Fw_Nav_Walker() by default
+ */
+function yamm_fw_roots_nav_menu_args($args = '')
+{
+    $yamm_fw_roots_nav_menu_args['container'] = false;
+
+    if (!$args['items_wrap'])
+    {
+        $yamm_fw_roots_nav_menu_args['items_wrap'] = '<ul class="%2$s">%3$s</ul>';
+    }
+
+    if (current_theme_supports('bootstrap-top-navbar') && !$args['depth'])
+    {
+        $yamm_fw_roots_nav_menu_args['depth'] = 3;
+    }
+
+    if (!$args['walker'])
+    {
+        $yamm_fw_roots_nav_menu_args['walker'] = new Yamm_Fw_Nav_Walker();
+    }
+
+    return array_merge($args, $yamm_fw_roots_nav_menu_args);
+}
+add_filter('wp_nav_menu_args', 'yamm_fw_roots_nav_menu_args');
+
+/**
+ * Fallback to simple list
+ */
+function Yamm_Fw_Nav_Walker_menu_fallback()
+{
+    $fb_output = '<ul class="nav navbar-nav">';
+    $fb_output .= '<li><a href="' . esc_url(home_url()) . '">' . __('Home', THEMETD) . '</a></li>';
+    if (current_user_can('manage_options'))
+    {
+        $fb_output .= '<li><a href="' . admin_url('nav-menus.php') . '">' . __('Add a menu', THEMETD) . '</a></li>';
+    }
+    $fb_output .= '</ul>';
+
+    echo $fb_output;
+}
+
+?>
